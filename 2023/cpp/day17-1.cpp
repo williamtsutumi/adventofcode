c9227106a8a95a2ad6b972ebc613e6108a686787
william <william.tsutumi@gmail.com>
2024-12-07 13:11:34 -0300

    day 17

--- 2023/cpp/day17-1.cpp
+++ 2023/cpp/day17-1.cpp
@@ -22,69 +22,34 @@ enum Direction{
     Up,Down,Left,Right
 };
 
-ll dp[150][150][4][5];
 vector<vector<int>> in;
 int lines, columns;
 
 ll solve(int x, int y, int straight, Direction direction);
 
-ll goUp(int x, int y, int straight){
-    return solve(x-1, y, straight, Up) + in[x][y];
+ll goUp(int x, int y, int straight, Direction direction){
+    return solve(x-1, y, straight, direction) + in[x][y];
 }
-
-ll goDown(int x, int y, int straight){
-    return solve(x+1, y, straight, Down) + in[x][y];
+ll goDown(int x, int y, int straight, Direction direction){
+    return solve(x+1, y, straight, direction) + in[x][y];
 }
-
-ll goRight(int x, int y, int straight){
-    return solve(x, y+1, straight, Right) + in[x][y];
+ll goLeft(int x, int y, int straight, Direction direction){
+    return solve(x, y-1, straight, direction) + in[x][y];
 }
-
-ll goLeft(int x, int y, int straight){
-    return solve(x, y-1, straight, Left) + in[x][y];
+ll goRight(int x, int y, int straight, Direction direction){
+    return solve(x, y+1, straight, direction) + in[x][y];
 }
 
 ll solve(int x, int y, int straight, Direction direction){
-    debug(x, y, straight, direction);
-    if (x == lines-1 && y == columns-1){
-        dp[x][y][straight][direction] = in[x][y];
-        return in[x][y];
-    } 
-    if (x < 0 || y < 0) return INF;
-    if (x >= lines || y >= columns) return INF;
-    if (straight > 3) return INF;
-    if (dp[x][y][straight][direction] != -1) return dp[x][y][straight][direction];
-
+    if (x == lines-1 && y == columns-1) return in[x][y];
+    debug(x, y);
 
     ll out = INF;
-    if (direction == Up || direction == Down){
-        out = min(out, goRight(x, y, 1) + out);
-    }
-    if (direction == Right){
-        out = min(out, goRight(x, y, straight + 1) + out);
-    }
-    if (direction == Left || direction == Right){
-        out = min(out, goDown(x, y, 1) + out);
-    }
-    if (direction == Down){
-        out = min(out, goDown(x, y, straight + 1) + out);
-    }
+    if (direction != Left && y+1 < columns) out = min(out, goRight(x, y, straight + 1, Right));
+    if (direction != Up && x+1 < lines) out = min(out, goDown(x, y, straight + 1, Down));
+    if (direction != Right && y-1 >= 0) out = min(out, goLeft(x, y, straight + 1, Left));
+    if (direction != Down && x-1 >= 0) out = min(out, goUp(x, y, straight + 1, Up));
 
-    if (direction == Left || direction == Right){
-        out = min(out, goUp(x, y, 1) + out);
-    }
-    if (direction == Up || direction == Down){
-        out = min(out, goLeft(x, y, 1) + out);
-    }
-    if (direction == Up){
-        out = min(out, goUp(x, y, straight + 1) + out);
-    }
-    if (direction == Left){
-        out = min(out, goLeft(x, y, straight + 1) + out);
-    }
-
-    dp[x][y][straight][direction] = out;
-    debug(out);
     return out;
 }
 
@@ -92,21 +57,17 @@ int main()
 {
     FFIO;
     string str;
-    vector<string> inp;
-    while (cin >> str) inp.push_back(str);
-    lines = inp.size();
-    columns = inp[0].size();
-
-    in.resize(inp.size());
-    for (int i=0; i<inp.size(); i++){
-        for (int j=0; j<inp[i].size(); j++){
-            in[i].push_back(inp[i][j] - '0');
+    while (cin >> str){
+        vector<int> line;
+        for (int i=0; i<str.size(); i++){
+            line.push_back(str[i] - '0');
         }
+        in.push_back(line);
     }
-
-    memset(dp, -1, sizeof(dp));
-    ll out = min(solve(0, 0, 1, Right), solve(0, 0, 1, Down));
-    debug(out);
+    lines = in.size();
+    columns = in[0].size();
+    debug(lines, columns);
+    cout << solve(0, 0, 1, Right) << endl;
 
     return 0;
 }
